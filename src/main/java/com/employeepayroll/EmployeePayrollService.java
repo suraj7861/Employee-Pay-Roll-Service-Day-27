4c040601047eec4c9576aa5e817a06200803c76c
unknown <khomanesuraj8@gmail.com>
2021-11-10 13:01:35 +0530

    Printed Welcome mesage

---
+++ src/main/java/com/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,9 @@
+package com.employeepayroll;
+
+public class EmployeePayrollService {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee payroll service program");
+	}
+
+}
